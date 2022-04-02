1c728aa36cb8a710166a5e98d069a6dc1648351f
LilCartell <lilian.cartellier@gmail.com>
2022-04-02 16:36:00 +0200

    First scene with slope and ball

---
+++ Assets/Scripts/Camera/CameraControl.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public Transform target;
+
+    private Vector3 _offsetToTarget;
+
+    private void Awake()
+    {
+        
+    }
+}
