84b73fc742952f1f4fa915321213129db9f331ba
Poupiste <cristowam@hotmail.fr>
2022-04-02 23:06:41 +0200

    Score Displayer

---
+++ Assets/ScoreCalculator.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreCalculator : MonoBehaviour
+{   
+    private float seconds;
+    private float tempScore = 0;
+    public int Score = 0;
+    public bool pause;
+
+    void Update () {
+        if(!pause){
+            seconds += Time.deltaTime;
+            tempScore = seconds * 20;
+            Score = (int) tempScore;
+            gameObject.GetComponent<Text>().text = Score + "";
+        }
+
+    }
+}
